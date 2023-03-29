898372bb6cebd476491ed7afbdce7e9981d7a647
Kennedy Owusu <kennediowusu@gmail.com>
2023-03-29 20:43:03 +0000

    display popular products in the right section of the UI

--- lib/widgets/build_carousel.dart
+++ lib/widgets/build_carousel.dart
@@ -1,6 +1,7 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:fashion_app/controller/product.dart';
 import 'package:fashion_app/model/products.dart';
+import 'package:fashion_app/widgets/loader.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -9,7 +10,7 @@ class BuildCarousel extends StatelessWidget {
 
   final double height;
 
-  ProductsController productController = Get.put(ProductsController());
+  final ProductsController productController = Get.put(ProductsController());
 
   // create a list of colors
   final List<Color> colors = [
@@ -21,43 +22,53 @@ class BuildCarousel extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CarouselSlider(
-      options: CarouselOptions(
-        height: height,
-        viewportFraction: 0.8,
-        autoPlay: true,
-        autoPlayInterval: Duration(seconds: 3),
-        autoPlayAnimationDuration: Duration(milliseconds: 800),
-        autoPlayCurve: Curves.fastOutSlowIn,
-        enlargeCenterPage: true,
-        scrollDirection: Axis.horizontal,
-      ),
-      items: [
-        ...productController.products.where(
-          (element) => element.isFeatured == Is.YES,
-        )
-      ].map((i) {
-        return Builder(
-          builder: (BuildContext context) {
-            return Container(
-              width: MediaQuery.of(context).size.width,
-              margin: EdgeInsets.symmetric(horizontal: 5.0),
-              decoration: BoxDecoration(
-                // color: colors[images.indexOf(i)].withOpacity(0.5),
-                color: colors[
-                        productController.products.indexOf(i) % colors.length]
-                    .withOpacity(0.5),
-                borderRadius: BorderRadius.circular(5),
-              ),
-
-              child: Image.network(
-                i.image,
-                fit: BoxFit.contain,
-              ),
-            );
-          },
-        );
-      }).toList(),
+    return Obx(
+      () {
+        return productController.isLoading.value
+            ? Loader()
+            : CarouselSlider(
+                options: CarouselOptions(
+                  height: height,
+                  viewportFraction: 0.8,
+                  autoPlay: true,
+                  autoPlayInterval: Duration(seconds: 3),
+                  autoPlayAnimationDuration: Duration(milliseconds: 800),
+                  autoPlayCurve: productController.products.length > 1
+                      ? Curves.fastOutSlowIn
+                      : Curves.linear,
+                  enlargeCenterPage: true,
+                  scrollDirection: Axis.horizontal,
+                ),
+                items: [
+                  ...productController.products.where(
+                    (element) => element.isFeatured == Is.YES,
+                  )
+                ].map(
+                  (i) {
+                    return Builder(
+                      builder: (BuildContext context) {
+                        return Container(
+                          width: MediaQuery.of(context).size.width,
+                          margin: EdgeInsets.symmetric(horizontal: 5.0),
+                          decoration: BoxDecoration(
+                            // color: colors[images.indexOf(i)].withOpacity(0.5),
+                            color: colors[
+                                    productController.products.indexOf(i) %
+                                        colors.length]
+                                .withOpacity(0.5),
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: Image.network(
+                            i.image,
+                            fit: BoxFit.contain,
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ).toList(),
+              );
+      },
     );
   }
 }
--- lib/widgets/build_popular_products.dart
+++ lib/widgets/build_popular_products.dart
@@ -1,7 +1,11 @@
+import 'package:fashion_app/controller/product.dart';
+import 'package:fashion_app/model/products.dart';
+import 'package:fashion_app/widgets/loader.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class BuildPopularProducts extends StatelessWidget {
-  const BuildPopularProducts({
+  BuildPopularProducts({
     super.key,
     required this.height,
     required this.width,
@@ -11,41 +15,61 @@ class BuildPopularProducts extends StatelessWidget {
   final double height, responsiveHeight;
   final double width;
 
+  final ProductsController productController = Get.put(ProductsController());
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 10),
+      margin: EdgeInsets.symmetric(horizontal: 10),
       height: responsiveHeight,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        physics: BouncingScrollPhysics(),
-        shrinkWrap: true,
-        itemCount: 5,
-        itemBuilder: (context, index) {
-          return Container(
-            height: 140,
-            margin: const EdgeInsets.only(right: 10),
-            child: Row(
-              children: [
-                Column(
-                  children: [
-                    Container(
-                      height: height,
-                      width: width,
-                      padding: const EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        color: Colors.grey[200],
-                        borderRadius: BorderRadius.circular(10),
+      child: Obx(
+        () {
+          final popularProducts = productController.products
+              .where((product) => product.isPopular == Is.YES)
+              .toList();
+
+          return productController.isLoading.value
+              ? Loader()
+              : ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  physics: BouncingScrollPhysics(),
+                  shrinkWrap: true,
+                  itemCount: popularProducts.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      height: 140,
+                      margin: const EdgeInsets.only(right: 10),
+                      child: Row(
+                        children: [
+                          Column(
+                            children: [
+                              Container(
+                                height: height,
+                                width: width,
+                                padding: const EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                  color: Colors.grey[200],
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: Image.network(
+                                  popularProducts[index].image,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              // SizedBox(height: 10),
+                              Text(
+                                popularProducts[index].name,
+                                style: TextStyle(
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
-                      child: Icon(Icons.access_alarm),
-                    ),
-                    SizedBox(height: 10),
-                    Text('Product $index'),
-                  ],
-                ),
-              ],
-            ),
-          );
+                    );
+                  },
+                );
         },
       ),
     );
