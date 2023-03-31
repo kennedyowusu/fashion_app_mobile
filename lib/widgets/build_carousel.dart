ca44d68f9781ae63ee1a3da6bc54b8330f9fee99
Kennedy Owusu <kennediowusu@gmail.com>
2023-03-31 18:02:33 +0000

    pass the selected product's index to the details screen

--- lib/widgets/build_carousel.dart
+++ lib/widgets/build_carousel.dart
@@ -13,7 +13,6 @@ class BuildCarousel extends StatelessWidget {
 
   final ProductsController productController = Get.put(ProductsController());
 
-  // create a list of colors
   final List<Color> colors = [
     Colors.red,
     Colors.green,
@@ -27,42 +26,39 @@ class BuildCarousel extends StatelessWidget {
       () {
         return productController.isLoading.value
             ? Loader()
-            : GestureDetector(
-                onTap: () {
-                  Get.to(
-                    () => ProductDetailsScreen(
-                      product: productController.products.firstWhere(
-                        (element) => element.isFeatured == Is.YES,
-                      ),
-                    ),
-                  );
-                },
-                child: CarouselSlider(
-                  options: CarouselOptions(
-                    height: height,
-                    viewportFraction: 0.8,
-                    autoPlay: true,
-                    autoPlayInterval: Duration(seconds: 3),
-                    autoPlayAnimationDuration: Duration(milliseconds: 800),
-                    autoPlayCurve: productController.products.length > 1
-                        ? Curves.fastOutSlowIn
-                        : Curves.linear,
-                    enlargeCenterPage: true,
-                    scrollDirection: Axis.horizontal,
-                  ),
-                  items: [
-                    ...productController.products.where(
-                      (element) => element.isFeatured == Is.YES,
-                    )
-                  ].map(
-                    (i) {
-                      return Builder(
-                        builder: (BuildContext context) {
-                          return Container(
+            : CarouselSlider(
+                options: CarouselOptions(
+                  height: height,
+                  viewportFraction: 0.8,
+                  autoPlay: true,
+                  autoPlayInterval: Duration(seconds: 3),
+                  autoPlayAnimationDuration: Duration(milliseconds: 800),
+                  autoPlayCurve: productController.products.length > 1
+                      ? Curves.fastOutSlowIn
+                      : Curves.linear,
+                  enlargeCenterPage: true,
+                  scrollDirection: Axis.horizontal,
+                ),
+                items: [
+                  ...productController.products.where(
+                    (element) => element.isFeatured == Is.YES,
+                  )
+                ].map(
+                  (i) {
+                    return Builder(
+                      builder: (BuildContext context) {
+                        return GestureDetector(
+                          onTap: () {
+                            Get.to(
+                              () => ProductDetailsScreen(
+                                product: i,
+                              ),
+                            );
+                          },
+                          child: Container(
                             width: MediaQuery.of(context).size.width,
                             margin: EdgeInsets.symmetric(horizontal: 5.0),
                             decoration: BoxDecoration(
-                              // color: colors[images.indexOf(i)].withOpacity(0.5),
                               color: colors[
                                       productController.products.indexOf(i) %
                                           colors.length]
@@ -73,12 +69,12 @@ class BuildCarousel extends StatelessWidget {
                               i.image,
                               fit: BoxFit.contain,
                             ),
-                          );
-                        },
-                      );
-                    },
-                  ).toList(),
-                ),
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ).toList(),
               );
       },
     );
