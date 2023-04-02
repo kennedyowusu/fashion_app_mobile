3bb52ef08c87ae8b203f512078f4fbcae27168b5
Kennedy Owusu <kennediowusu@gmail.com>
2023-04-02 09:14:42 +0000

    handle dynamic navigation to the product detail screen

--- lib/widgets/build_product_list.dart
+++ lib/widgets/build_product_list.dart
@@ -1,6 +1,7 @@
 import 'package:fashion_app/controller/product.dart';
 import 'package:fashion_app/helper/build_product_cart.dart';
 import 'package:fashion_app/model/products.dart';
+import 'package:fashion_app/views/details/details.dart';
 import 'package:fashion_app/widgets/appbar.dart';
 import 'package:fashion_app/widgets/loader.dart';
 import 'package:flutter/material.dart';
@@ -52,58 +53,65 @@ class ProductListScreen extends StatelessWidget {
                     final product = products[index];
                     return Padding(
                       padding: EdgeInsets.all(8.0),
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(
-                            width: 100,
-                            height: 80,
-                            child: Image.network(
-                              product.image,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                          SizedBox(width: 16),
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  product.name,
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.w400,
-                                    fontSize: 16,
-                                  ),
-                                ),
-                                SizedBox(height: 8),
-                                Text(
-                                  'GHS ${product.price}',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 16,
-                                    color: Colors.green,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          CircleAvatar(
-                            radius: 20,
-                            backgroundColor: Colors.orange,
-                            child: Center(
-                              child: IconButton(
-                                icon: Icon(
-                                  Icons.shopping_cart,
-                                  color: Colors.white,
-                                  size: 16,
-                                ),
-                                onPressed: () {
-                                  // Handle adding to cart
-                                },
+                      child: GestureDetector(
+                        onTap: () {
+                          isPopular
+                              ? Get.to(ProductDetailsScreen(product: product))
+                              : Get.to(ProductDetailsScreen(product: product));
+                        },
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              width: 100,
+                              height: 80,
+                              child: Image.network(
+                                product.image,
+                                fit: BoxFit.cover,
                               ),
                             ),
-                          ),
-                        ],
+                            SizedBox(width: 16),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    product.name,
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w400,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                  SizedBox(height: 8),
+                                  Text(
+                                    'GHS ${product.price}',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16,
+                                      color: Colors.green,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            CircleAvatar(
+                              radius: 20,
+                              backgroundColor: Colors.orange,
+                              child: Center(
+                                child: IconButton(
+                                  icon: Icon(
+                                    Icons.shopping_cart,
+                                    color: Colors.white,
+                                    size: 16,
+                                  ),
+                                  onPressed: () {
+                                    // Handle adding to cart
+                                  },
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     );
                   },
