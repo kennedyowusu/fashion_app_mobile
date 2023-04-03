205a704b6f3611afea8d68d1bd438a2e412e37b8
Kennedy Owusu <kennediowusu@gmail.com>
2023-04-03 08:52:27 +0000

    refactor the cart items code

--- lib/views/cart/cart.dart
+++ lib/views/cart/cart.dart
@@ -14,121 +14,142 @@ class CartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: CustomAppBar(
+        title: 'Cart Items',
+        leadingIcon: FaIcon(
+          FontAwesomeIcons.chevronLeft,
+          color: Colors.black,
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                color: Colors.white,
+              ),
+              child: Icon(
+                Icons.favorite_border,
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ],
+      ),
       body: Obx(
-        () => cartController.isLoading.value
-            ? Center(
-                child: Text('No items in cart'),
-              )
-            : Obx(
-                () => ListView.separated(
-                  itemCount: cartController.cartItems.length,
-                  separatorBuilder: (context, index) => Divider(),
-                  itemBuilder: (context, index) {
-                    final cartItem = cartController.cartItems[index];
-                    TextStyle textStyle = TextStyle(
-                      fontFamily: 'Montserrat',
-                      fontWeight: FontWeight.bold,
-                    );
-                    return Dismissible(
-                      key: Key(cartItem.id.toString()),
-                      direction: DismissDirection.endToStart,
-                      onDismissed: (direction) {
-                        // Remove the item from the cart
-                        cartController.removeCartItem(index);
-                      },
-                      background: Container(
-                        color: Colors.red,
-                        child: Padding(
-                          padding: const EdgeInsets.only(right: 16.0),
-                          child: Align(
-                            alignment: Alignment.centerRight,
-                            child: Icon(Icons.delete, color: Colors.white),
-                          ),
-                        ),
+        () {
+          if (cartController.isLoading.value) {
+            return Center(child: CircularProgressIndicator());
+          } else if (cartController.cartItems.isEmpty) {
+            return Center(child: Text('Your cart is empty.'));
+          } else {
+            return ListView.separated(
+              itemCount: cartController.cartItems.length,
+              separatorBuilder: (context, index) => Divider(),
+              itemBuilder: (context, index) {
+                final cartItem = cartController.cartItems[index];
+                TextStyle textStyle = TextStyle(
+                  fontFamily: 'Montserrat',
+                  fontWeight: FontWeight.bold,
+                );
+                return Dismissible(
+                  key: Key(cartItem.id.toString()),
+                  direction: DismissDirection.endToStart,
+                  onDismissed: (direction) {
+                    // Remove the item from the cart
+                    cartController.removeCartItem(index);
+                  },
+                  background: Container(
+                    color: Colors.red,
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 16.0),
+                      child: Align(
+                        alignment: Alignment.centerRight,
+                        child: Icon(Icons.delete, color: Colors.white),
                       ),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 16.0,
-                          vertical: 8.0,
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0,
+                      vertical: 8.0,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          flex: 2,
+                          child: Image.network(cartItem.image),
                         ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Expanded(
-                              flex: 2,
-                              child: Image.network(cartItem.image),
-                            ),
-                            SizedBox(width: 16.0),
-                            Expanded(
-                              flex: 3,
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
+                        SizedBox(width: 16.0),
+                        Expanded(
+                          flex: 3,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                cartItem.name,
+                                style: textStyle,
+                              ),
+                              SizedBox(
+                                height: 8.0,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.center,
                                 children: [
+                                  _buildRoundIconButton(
+                                    icon: Icon(
+                                      Icons.remove,
+                                      size: 18.0,
+                                      color: Colors.white,
+                                    ),
+                                    onPressed: () {
+                                      cartController.decrementQuantity(index);
+                                    },
+                                  ),
+                                  const SizedBox(width: 8.0),
                                   Text(
-                                    cartItem.name,
-                                    style: textStyle,
+                                    cartItem.quantity.toString(),
+                                    style: const TextStyle(
+                                      fontSize: 18.0,
+                                      fontFamily: 'Montserrat',
+                                      fontWeight: FontWeight.bold,
+                                    ),
                                   ),
-                                  SizedBox(
-                                    height: 8.0,
-                                  ),
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.center,
-                                    children: [
-                                      _buildRoundIconButton(
-                                        icon: Icon(
-                                          Icons.remove,
-                                          size: 18.0,
-                                          color: Colors.white,
-                                        ),
-                                        onPressed: () {
-                                          cartController
-                                              .decrementQuantity(index);
-                                        },
-                                      ),
-                                      const SizedBox(width: 8.0),
-                                      Text(
-                                        cartItem.quantity.toString(),
-                                        style: const TextStyle(
-                                          fontSize: 18.0,
-                                          fontFamily: 'Montserrat',
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                      const SizedBox(width: 8.0),
-                                      _buildRoundIconButton(
-                                        icon: Icon(
-                                          Icons.add,
-                                          color: Colors.white,
-                                          size: 18.0,
-                                        ),
-                                        onPressed: () {
-                                          cartController
-                                              .incrementQuantity(index);
-                                        },
-                                      ),
-                                    ],
+                                  const SizedBox(width: 8.0),
+                                  _buildRoundIconButton(
+                                    icon: Icon(
+                                      Icons.add,
+                                      color: Colors.white,
+                                      size: 18.0,
+                                    ),
+                                    onPressed: () {
+                                      cartController.incrementQuantity(index);
+                                    },
                                   ),
                                 ],
                               ),
-                            ),
-                            Expanded(
-                              flex: 1,
-                              child: Text(
-                                '₵${(cartItem.price * cartItem.quantity)}',
-                                textAlign: TextAlign.right,
-                                style: textStyle,
-                              ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
-                    );
-                  },
-                ),
-              ),
+                        Expanded(
+                          flex: 1,
+                          child: Text(
+                            '₵${(cartItem.price * cartItem.quantity)}',
+                            textAlign: TextAlign.right,
+                            style: textStyle,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            );
+          }
+        },
       ),
       bottomNavigationBar: BottomAppBar(
         child: Container(
