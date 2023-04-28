162c8fd401cd6cc08c5b2d82ef8fe53532e050cf
Kennedy Owusu <kennediowusu@gmail.com>
2023-04-28 15:39:37 +0000

    work on shipping address deletion

--- lib/views/checkout/shipping_address_list.dart
+++ lib/views/checkout/shipping_address_list.dart
@@ -8,14 +8,12 @@ import 'package:fashion_app/widgets/appbar.dart';
 import 'package:fashion_app/widgets/button.dart';
 import 'package:fashion_app/widgets/loader.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 
 class ShippingAddressList extends StatelessWidget {
-  final ShippingAddressController shippingAddressService =
-      Get.put(ShippingAddressController());
-
   final ShippingAddressController shippingAddressController =
       Get.put(ShippingAddressController());
 
@@ -77,46 +75,154 @@ class ShippingAddressList extends StatelessWidget {
                           itemBuilder: (context, index) {
                             ShippingAddress address = shippingAddressController
                                 .shippingAddress[index];
-                            return GestureDetector(
-                              onTap: () {
-                                Get.to(
-                                  () => PaymentScreen(),
+                            return Dismissible(
+                              key: Key(address.id.toString()),
+                              onDismissed: (direction) async {
+                                bool confirmDeletion = await showDialog(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    return AlertDialog(
+                                      title: Row(
+                                        children: [
+                                          Container(
+                                            padding: EdgeInsets.all(5),
+                                            decoration: BoxDecoration(
+                                              borderRadius:
+                                                  BorderRadius.circular(5),
+                                              border: Border.all(
+                                                color: Colors.yellow,
+                                              ),
+                                            ),
+                                            child: Icon(
+                                              Icons.warning,
+                                              color: Colors.yellow,
+                                              size: 30.0,
+                                            ),
+                                          ),
+                                          SizedBox(width: 10.0),
+                                          Text(
+                                            'Confirm Deletion'.toUpperCase(),
+                                            style: TextStyle(
+                                              fontSize: 16.0,
+                                              fontWeight: FontWeight.w500,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                      content: Text(
+                                        textAlign: TextAlign.center,
+                                        'Are you sure you want to delete this shipping address?',
+                                        style: TextStyle(
+                                          fontSize: 16.0,
+                                        ),
+                                      ),
+                                      actions: [
+                                        TextButton(
+                                          style: TextButton.styleFrom(
+                                            foregroundColor: Colors.green,
+                                            shape: RoundedRectangleBorder(
+                                              borderRadius:
+                                                  BorderRadius.circular(5),
+                                            ),
+                                          ),
+                                          child: Text('Cancel'),
+                                          onPressed: () {
+                                            Navigator.of(context)
+                                                .pop(false); // Do not delete
+                                          },
+                                        ),
+                                        TextButton(
+                                          style: TextButton.styleFrom(
+                                            foregroundColor: Colors.red,
+                                          ),
+                                          child: Text('Delete'),
+                                          onPressed: () {
+                                            Navigator.of(context).pop(true);
+                                          },
+                                        ),
+                                      ],
+                                    );
+                                  },
                                 );
-                                debugPrint(
-                                  'Shipping Address: ${address.toJson()}',
-                                );
+
+                                if (confirmDeletion) {
+                                  try {
+                                    await shippingAddressController
+                                        .deleteShippingAddress(id: address.id);
+
+                                    Fluttertoast.showToast(
+                                      msg: 'Shipping Address Deleted',
+                                      toastLength: Toast.LENGTH_SHORT,
+                                      gravity: ToastGravity.BOTTOM,
+                                    );
+                                  } catch (e) {
+                                    debugPrint('Error: $e');
+                                    Fluttertoast.showToast(
+                                      msg:
+                                          'Failed to delete the shipping address',
+                                      toastLength: Toast.LENGTH_SHORT,
+                                      gravity: ToastGravity.BOTTOM,
+                                    );
+                                  }
+                                }
+                                // Remove the dismissed item from the list
+                                shippingAddressController.shippingAddress
+                                    .removeAt(index);
                               },
-                              child: Container(
-                                height: 100,
-                                width: double.infinity,
-                                margin: EdgeInsets.all(16),
+                              background: Container(
+                                padding: EdgeInsets.symmetric(horizontal: 16),
                                 decoration: BoxDecoration(
                                   borderRadius: BorderRadius.circular(16),
-                                  color: Config.primaryColor,
+                                  color: Colors.red,
                                 ),
-                                child: ListTile(
-                                  title: Text(
-                                    address.name,
-                                    style: TextStyle(
-                                      fontSize: 18,
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  subtitle: Text(
-                                    '${address.addressLine1}, ${address.city} ${address.state} ${address.zip}',
-                                    style: TextStyle(
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    Icon(
+                                      Icons.delete,
                                       color: Colors.white,
                                     ),
+                                  ],
+                                ),
+                              ),
+                              child: GestureDetector(
+                                onTap: () {
+                                  Get.to(() => PaymentScreen());
+                                  debugPrint(
+                                      'Shipping Address: ${address.toJson()}');
+                                },
+                                child: Container(
+                                  height: 100,
+                                  width: double.infinity,
+                                  margin: EdgeInsets.all(16),
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(16),
+                                    color: Config.primaryColor,
                                   ),
-                                  trailing: CircleAvatar(
-                                    backgroundColor: Colors.white,
-                                    radius: 20,
-                                    child: IconButton(
-                                      icon: Icon(Icons.edit),
-                                      onPressed: () {
-                                        // Navigate to Edit Shipping Address screen
-                                      },
+                                  child: ListTile(
+                                    title: Text(
+                                      address.name,
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    subtitle: Text(
+                                      '${address.addressLine1}, ${address.city} ${address.state} ${address.zip}',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                    trailing: CircleAvatar(
+                                      backgroundColor: Colors.white,
+                                      radius: 20,
+                                      child: IconButton(
+                                        icon: Icon(Icons.edit),
+                                        onPressed: () {
+                                          // Navigate to Edit Shipping Address screen
+                                        },
+                                      ),
                                     ),
                                   ),
                                 ),
